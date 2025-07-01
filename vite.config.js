06274dce6b81d930076c0a2305e5954aaf06f08b
Dodge <annakrtska@gmail.com>
2025-07-01 04:04:38 +0300

    fix3

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react-swc';
 
+// 🔥 Replace `Test-App-Inforce` with your actual repo name
 export default defineConfig({
   plugins: [react()],
   base: '/Test-App-Inforce/',
